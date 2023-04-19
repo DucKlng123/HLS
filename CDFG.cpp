3811f4a5ad925fcbf9403cdd96870296bf247e08
DucKlng123 <94210275+DucKlng123@users.noreply.github.com>
2023-04-19 22:45:21 +0800

    Add files via upload

--- CDFG.cpp
+++ CDFG.cpp
@@ -3,21 +3,29 @@
 std::vector<BasicBlock> &getBasicBlocks(std::vector<basic_block> bbs){
     //遍历每个basic_block的语句，遇到br,设置前驱后继
 
-    std::vector<BasicBlock> BB;
     std::map<std::string,BasicBlock> BBLable;
     for(auto bb:bbs){
         BBLable[bb.get_label_name()] = BasicBlock(bb);
     }
     for(auto bb : BBLable){
-        for(auto s : bb.second.get_statements()){
+        BasicBlock &BB = bb.second;
+        for(auto s : BB.get_statements()){
             int type = s.get_type();
             if(type == OP_BR){
                 int numOperands = s.get_num_oprands();
                 if(numOperands == 1){//无条件跳转   
-
+                    //设置successor和predcessor
+                    BasicBlock &succ = BBLable[s.get_oprand(0)];
+                    BB.successors.push_back(&succ);
+                    succ.predecessors.push_back(&BB);
                 }
                 else if(numOperands == 3){//条件分支
-
+                    BasicBlock &succ1 = BBLable[s.get_oprand(1)];
+                    BasicBlock &succ2 = BBLable[s.get_oprand(2)];
+                    BB.successors.push_back(&succ1);
+                    BB.successors.push_back(&succ2);
+                    succ1.predecessors.push_back(&BB);
+                    succ2.predecessors.push_back(&BB);
                 }
                 else{
                     std::cout<<"err br op!"<<std::endl;
@@ -25,4 +33,3 @@ std::vector<BasicBlock> &getBasicBlocks(std::vector<basic_block> bbs){
             }
         }
     }
-}
